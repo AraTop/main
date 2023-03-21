db96f4a614c380c116b2c54ef441963cfce8b5aa
AraTop <119860838+AraTop@users.noreply.github.com>
2023-03-21 12:20:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from test import  Store, Shop, Request , Storage
+
+
+
+a = [
+   {"item":"яблоко",
+   "value":10
+   },
+   {"item":"груша",
+   "value":6
+   },
+   {"item":"апельсин",
+   "value":9
+   }
+]
+
+store = Store(a)
+shop = Shop(a)
+request = Request("склада","магазин",3,"грушы")
+print(request.__repr__())
+shop.add("мясо", 1)
+print(shop.get_unique_items_count())
+print(shop.get_items())
+print(shop.get_free_space())
