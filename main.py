16c612a9f5da28caed0cabb989d2af9200a12102
AraTop <119860838+AraTop@users.noreply.github.com>
2023-03-25 16:46:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from test import  Store, Shop, Request , Storage ,a
+
+
+request = Request(input())
+print(request.__repr__())
+#print(shop.get_unique_items_count())
+#print(shop.get_items())
+#print(shop.get_free_space())
+
